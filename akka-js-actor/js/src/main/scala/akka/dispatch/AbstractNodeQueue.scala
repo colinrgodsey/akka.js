b6e1e9b6839d2e9480c91bcbceb3f97ed637ad02
Colin Godsey <crgodsey@gmail.com>
2016-07-15 12:12:10 -0600

    adding enqueueFirst to mailbox
    needed for UnboundedDequeBasedMessageQueueSemantics semantics, needed
    for a functioning stash.

--- akka-js-actor/js/src/main/scala/akka/dispatch/AbstractNodeQueue.scala
+++ akka-js-actor/js/src/main/scala/akka/dispatch/AbstractNodeQueue.scala
@@ -5,6 +5,13 @@ import scala.collection.mutable.Queue
 class AbstractNodeQueue[A >: Null] extends Queue[A] {
   private[dispatch] def add(value: A) = super.enqueue(value)
 
+  private[dispatch] def addFirst(value: A): Unit = {
+    val tailPart = dequeueAll(_ => true)
+
+    add(value)
+    tailPart foreach add
+  }
+
   private[dispatch] def poll(): A = if(isEmpty) null else super.dequeue()
 
   private[dispatch] def count() = size
